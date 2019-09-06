51c8f8241dd6a372623abf979b68971614eacca6
Nathaniel Mosco <natemosco@gmail.com>
2019-09-06 11:38:48 -0400

    header function created and appended to the HTML

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,26 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+const headerContainer = document.querySelector(".header-container");
 
-function Header() {}
+function headerMaker() {
+    const header = document.createElement("div");
+    const date = document.createElement("span");
+    const title = document.createElement("h1");
+    const temp = document.createElement("span");
+
+    header.append(date);
+    header.append(title);
+    header.append(temp);
+
+    header.classList.add("header");
+    date.classList.add("date");
+    temp.classList.add("temp");
+
+    date.textContent = "SMARCH 28, 2019";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°"
+
+    return header;
+}
+headerContainer.append(headerMaker())
